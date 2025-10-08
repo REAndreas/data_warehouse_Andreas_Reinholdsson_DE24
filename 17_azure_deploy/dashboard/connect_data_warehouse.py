c96c67038fe9c80ae930d625784be8b9da67c546
Andreas <andreas.reinhol@gmail.com>
2025-10-08 16:43:43 +0200

    changes to the azure deploy

--- 17_azure_deploy/dashboard/connect_data_warehouse.py
+++ 17_azure_deploy/dashboard/connect_data_warehouse.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import duckdb
 import os 
 
-DB_PATH = os.getenv("DUCKDB_PATH")
+DB_PATH = Path("/mnt/data/job_ads.duckdb")
  
 def query_job_listings(query='SELECT * FROM marts.mart_technical_jobs'):
     with duckdb.connect(DB_PATH, read_only=True) as conn:
